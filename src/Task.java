3dc93daf7ad534dbec88e7fd81683deba63f38b0
Rubaha <sania.bubnov@mail.ru>
2025-03-11 20:58:26 +0300

    Переменная status класса Task теперь приватная.

--- src/Task.java
+++ src/Task.java
@@ -4,7 +4,7 @@ public class Task {
     private String title;
     private String description;
     private int id;
-    Status status;
+    private Status status;
 
     public Task(String title, String description, int id, Status status) {
         this.title = title;
